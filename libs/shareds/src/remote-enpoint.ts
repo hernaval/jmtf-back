8b16d125b6f3e1a837fbac64ac5a8f296b45182d
Herinavalona <hernavalasco@gmail.com>
2021-09-12 15:03:50 +0300

    creating next payment schema, dto, api

--- libs/shareds/src/remote-enpoint.ts
+++ libs/shareds/src/remote-enpoint.ts
@@ -1,2 +1,3 @@
 const BASE_URI = 'https://admin-paiements.thejohnmaxwellteam.fr/api';
 export const ALL_PAYMENT_URL = `${BASE_URI}/paiements/all.php`;
+export const ALL_NEXT_PAYMENT_URL = `${BASE_URI}/paiements/next.php`;
